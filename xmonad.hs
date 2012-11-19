e2e0b811679ea943e2e4ea79ec1a75af2f7e793f
Geoff Liu <cangming.liu@gmail.com>
2012-11-18 23:57:21 -0500

    xmonad tweaks

--- xmonad.hs
+++ xmonad.hs
@@ -4,4 +4,7 @@ main = do
     xmonad $ defaultConfig
         { terminal = "urxvt"
         , focusFollowsMouse = False
+        , modMask = mod4Mask
+        , focusedBorderColor = "#9fee00"
+        , normalBorderColor = "#85004b"
         }
