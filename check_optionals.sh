e710f7b12933281f53b510bc385b129fe4ef34f5
Geoff Liu <hi@geoffliu.me>
2021-03-19 17:33:23 +0300

    Set Carbon Stasis to 3

--- check_optionals.sh
+++ check_optionals.sh
@@ -1,12 +1,10 @@
 #!/bin/zsh
 
 command -V fcitx
-command -V nm-applet
-command -V pamixer
+command -V pactl
 command -V sxiv
 command -V scrot
 command -V xclip
-command -V checkupdates
 
 Fonts=(
   'Noto Sans'
